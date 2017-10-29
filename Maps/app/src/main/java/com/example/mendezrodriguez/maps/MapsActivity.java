3ee103129bfecaa781ac8f6a708ae2dd8867faf1
maggie97 <maggielee97@icloud.com>
2017-10-28 23:51:07 -0500

    mapa
    Se agregaron los typos de mapas como hybrido, satelite, sin mapa,
    normal y terreno
    
    se agregaron 2 marcadores

--- Maps/app/src/main/java/com/example/mendezrodriguez/maps/MapsActivity.java
+++ Maps/app/src/main/java/com/example/mendezrodriguez/maps/MapsActivity.java
@@ -1,22 +1,30 @@
 package com.example.mendezrodriguez.maps;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-
+    private static final LatLng MENLYN_PRETORIA =  new LatLng(-25.782420, 28.275439);
+    private static final LatLng PROTEA_HOTEL = new LatLng(-25.786508, 28.271187);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +32,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        mapFragment.newInstance(new GoogleMapOptions());
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -43,17 +53,41 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory
+                .fromResource(R.mipmap.hotel)).anchor(0.0f, 1.0f).title("Protea Hotel").position(PROTEA_HOTEL));
+        mMap.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory
+                .fromResource(R.mipmap.mall)).anchor(0.0f, 1.0f).title("Menlyn Mall").position(MENLYN_PRETORIA));
+
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.map, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_normal:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.action_hybrid:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            case R.id.action_satellite:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.action_terrain:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case R.id.action_none:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
